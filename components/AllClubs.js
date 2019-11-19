fcfa6aa9d75735f45163c067c01ed00ff0137ceb
AumUpadhyay <aumspeaks@gmail.com>
2019-11-18 21:34:53 -0500

    added file

--- components/AllClubs.js
+++ components/AllClubs.js
@@ -1,26 +1,116 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  StyleSheet,
+  Text,
+  ScrollView,
+} from 'react-native';
+import { Card, SearchBar } from 'react-native-elements';
+
+const DATA = [
+  {
+    website: 'www.FirstClub.org',
+    key: 'First Club',
+    description:
+      'this is a random description for a club that is interesting and is on ClubSwipe',
+  },
+  {
+    website: 'www.SecondClub.org',
+    key: 'Second Club',
+    description:
+      'this is a random description for a club that is interesting and is on ClubSwipe',
+  },
+  {
+    website: 'www.ThirdClub.org',
+    key: 'Third Club',
+    description:
+      'this is a random description for a club that is interesting and is on ClubSwipe',
+  },
+  {
+    website: 'www.FourthClub.org',
+    key: 'Fourth Club',
+    description:
+      'this is a random description for a club that is interesting and is on ClubSwipe',
+  },
+  {
+    website: 'www.FifthClub.org',
+    key: 'Fifth Club',
+    description:
+      'this is a random description for a club that is interesting and is on ClubSwipe',
+  },
+  {
+    website: 'www.SixthClub.org',
+    key: 'Sixth Club',
+    description:
+      'this is a random description for a club that is interesting and is on ClubSwipe',
+  },
+];
+
 
 export default class AllClubs extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
 
-    render() {
-        return (
-        <View style={styles.container}>
-            <Text>{JSON.stringify(this.props.clubs)}</Text>
-            <Button title={'Back'} onPress={this.props.dismiss} />
-        </View>
-        );
+    this.state = {
+      search: "Search For Clubs",
+      websites: [],
+      descriptions: [],
+      clubName: [],
+      data: [],
+    };
+  }
+
+  componentWillMount() {
+    this.setState({
+      data: DATA,
+    });
+  }
+  updateSearch = search => {
+    this.setState({
+      search,
+    });
+  };
+
+  executeSearch = search => {
+    for (let i = 0; i < this.state.length; i++) {
+      console.log('Here');
     }
+  };
+
+  render() {
+    return (
+      <ScrollView>
+        <View style={styles.searchbarStyle}>
+          <SearchBar
+            placeholder="Search For Clubs"
+            value = {this.state.search}
+            onChangeText={this.updateSearch}
+          />
+        </View>
+        <SafeAreaView style={styles.container}>
+          <FlatList
+            data={this.state.data}
+            renderItem={({ item }) => (
+              <Card title={item.key}>
+                <Text style={styles.website}>{item.website}</Text>
+                <Text>{item.description}</Text>
+              </Card>
+            )}
+            keyExtractor={item => item.key}
+          />
+        </SafeAreaView>
+      </ScrollView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+  container: {
+    flex: 1,
+  },
+  website: {
+    color: 'blue',
+  },
+});

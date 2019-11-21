7f2870450a18d7ea4ffa605e304754186fe84609
Raghav Mecheri <raghav.mecheri@columbia.edu>
2019-11-20 22:53:15 -0500

    Presentable splash + landing page

--- App.js
+++ App.js
@@ -40,11 +40,13 @@ const recomendedClubMock = [
 ];
 
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView} from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image} from 'react-native';
 import Button from "react-native-button"
 import Login from "./components/Login"
 import SignUp from "./components/SignUp"
 import Main from "./components/Main"
+
+const logoURL = "./assets/bigLogo.png";
 
 export default class App extends React.Component {
   constructor() {
@@ -92,11 +94,9 @@ export default class App extends React.Component {
       return (
         <SafeAreaView style={styles.container}>
             <View style={styles.logoHold}>
+              <Image source={require(logoURL)} style={styles.logo}/>
             </View>
             <View style={styles.buttonHold}>
-              <Text style={styles.caption}>
-                Find your new club today
-              </Text>
               <Button containerStyle={styles.buttonContainer} style={styles.buttonStyle} onPress={() => this.setState({renderLogin:true})}>Login</Button>
               <Button containerStyle={styles.buttonContainer} style={styles.buttonStyle} onPress={() => this.setState({renderSignup:true})}>Sign Up</Button>
             </View>
@@ -114,7 +114,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   logoHold: {
-    flex: 3
+    flex: 3,
+    paddingTop: 110
+  },
+  logo: {
+    height: 250,
+    width: 250
   },
   buttonHold: {
     flex: 2,
@@ -124,16 +129,14 @@ const styles = StyleSheet.create({
   buttonContainer: {
     height: 50,
     width: 250,
-    marginTop:15,
-    backgroundColor:"#0974ed",
+    marginTop:23,
+    borderRadius:3,
+    backgroundColor:"#2FA6FF",
     overflow:'hidden',
     justifyContent: 'center'
   },
   buttonStyle: {
     fontSize: 15,
     color: "white",
-  },
-  caption: {
-    marginBottom:15,
   }
 });
